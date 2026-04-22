e66f7f3d0a7bb8c86f9884b14e38f852dc4bbf83
msmith71763-beep <msmith71763@gmail.com>
2026-04-22 07:24:17 -0400

    Stability Test: Simplest possible Viewport

--- src/components/3d/Viewport.tsx
+++ src/components/3d/Viewport.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
 import BaseModel from "./BaseModel";
-import type { CustomizationState } from "@/types/customization";
 
-export default function Viewport({ customization }: { customization: CustomizationState }) {
+export default function Viewport() {
   return (
     <div className="w-full h-full bg-[#0a0a0c]">
       <Canvas dpr={1}>
-        <PerspectiveCamera makeDefault position={[0, 1.2, 4]} />
+        <PerspectiveCamera makeDefault position={[0, 0, 5]} />
         <color attach="background" args={["#0a0a0c"]} />
-        <ambientLight intensity={1.5} />
+        <ambientLight intensity={1} />
+        <pointLight position={[10, 10, 10]} />
         
-        <Suspense fallback={null}>
-          <BaseModel customization={customization} />
-          <OrbitControls target={[0, 0.8, 0]} />
-        </Suspense>
+        <BaseModel />
+        <OrbitControls />
       </Canvas>
     </div>
   );
